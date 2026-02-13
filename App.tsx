1e34b42ff7a7e6592c32dbf0ef83bc27d579249b
Hammad Arshad <136149262+Jathol7@users.noreply.github.com>
2026-02-13 16:44:11 +0500

    add floating whatsapp button

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import Blogs from './pages/Blogs';
 import BlogDetail from './pages/BlogDetail';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsConditions from './pages/TermsConditions';
+import WhatsAppButton from './components/WhatsAppButton';
 const ScrollToTop = () => {
   const { pathname } = useLocation();
   
@@ -50,6 +51,7 @@ const App: React.FC = () => {
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
+        <WhatsAppButton />
         <Footer />
       </div>
     </Router>
